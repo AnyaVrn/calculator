18419f8a020de5d1780defae64be627aaa297829
AnyaVrn <anya.voronina95@gmail.com>
2022-11-03 04:08:23 +0300

    Second commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,11 @@
 public class CreditPaymentService {
-    public int calculate(double summa, double procent, double srok) {
+    public int calculate(double sum, double procent, double time) {
         double a = 1 + procent;
-        double b = Math.pow(a, srok);
+        double b = Math.pow(a, time);
         double c = (b - 1);
         double d = procent / c;
         double e = procent + d;
-        double result = summa * e;
+        double result = sum * e;
         return (int) result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double summa = 1_000_000;
+        double sum = 1_000_000;
         double rate = 0.0999;
         double percent = rate / 12;
-        double srok = 36;
-        double х = service.calculate(summa, percent, srok);
+        double loan = 36;
+        double х = service.calculate(sum, percent, loan);
         System.out.println(х);
     }
 }
